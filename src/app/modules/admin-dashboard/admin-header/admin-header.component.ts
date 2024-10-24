ed9f24e05072fbb35bb6331f8ad74fb95411c7ba
Izzeldeen Ashour <ezraashour00@gmail.com>
2024-10-24 13:24:29 +0300

    push

--- src/app/modules/admin-dashboard/admin-header/admin-header.component.ts
+++ src/app/modules/admin-dashboard/admin-header/admin-header.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-
+import { SidebarService } from '../admin-services/sidebar.service';
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
   styleUrls: ['./admin-header.component.scss']
 })
 export class AdminHeaderComponent {
-  constructor(){
+  constructor(private sidebarService: SidebarService) {}
 
+  toggleMobileSideBar(): void {
+    this.sidebarService.toggleSidebar(); // Use service to toggle the sidebar
   }
-
-  public toggleMobileSideBar(): void {
-    // this.sideBar.switchMobileSideBarPosition();
-  }
-
 }
