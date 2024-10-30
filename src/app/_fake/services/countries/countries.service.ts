9eb29575778847d0f30c9ace1cf42ee47833ed41
Izzeldeen Ashour <ezraashour00@gmail.com>
2024-10-30 12:03:58 +0300

    push

--- src/app/_fake/services/countries/countries.service.ts
+++ src/app/_fake/services/countries/countries.service.ts
@@ -28,9 +28,9 @@ export interface Country {
   providedIn: 'root'
 })
 export class CountriesService {
-  private apiUrl = 'https://api.4sighta.com/api/admin/setting/country/list';
-  private createApi = 'https://api.4sighta.com/api/admin/setting/country';
-  private updateDeleteApi = 'https://api.4sighta.com/api/admin/setting/country';
+  private apiUrl = 'https://api.4sighta.com/api/common/setting/country/list';
+  private createApi = 'https://api.4sighta.com/api/common/setting/country';
+  private updateDeleteApi = 'https://api.4sighta.com/api/common/setting/country';
   private isLoadingSubject = new BehaviorSubject<boolean>(false);
   public isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
   currentLang: string = 'en';
