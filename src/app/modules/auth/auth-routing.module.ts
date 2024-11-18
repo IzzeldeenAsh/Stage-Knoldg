7f280a524e589f8b59d9fe56dc3e2604e5d02528
Izzeldeen Ashour <ezraashour00@gmail.com>
2024-11-18 10:38:14 +0300

    push

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent,
         data: { returnUrl: window.location.pathname },
+        canActivate:[UnAuthGuard]
       },
       {
         path:'verify-email',
