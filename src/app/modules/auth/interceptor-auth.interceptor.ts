d6adb20b26c6c7cd9d8d930eaf0a6f5a43da5fe2
Izzeldeen Ashour <ezraashour00@gmail.com>
2024-12-26 16:58:46 +0300

    pushing

--- src/app/modules/auth/interceptor-auth.interceptor.ts
+++ src/app/modules/auth/interceptor-auth.interceptor.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { catchError, first, Observable, throwError } from 'rxjs';
 import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,private router:Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const auth = this.authService.getAuthFromLocalStorage();
@@ -20,6 +21,27 @@ export class AuthInterceptor implements HttpInterceptor {
       headers: req.headers.set('Authorization', `Bearer ${auth.authToken}`)
     });
 
-    return next.handle(clonedReq); // Pass the cloned request with headers to the next handler
+    return next.handle(clonedReq).pipe(
+      catchError((error:HttpErrorResponse)=>{
+        if(error.status === 401){
+          this.authService.logout().pipe(
+            first()
+          ).subscribe({
+            next: () => {
+              localStorage.removeItem("foresighta-creds");
+              localStorage.removeItem("user");
+              this.router.navigate(['/auth']).then(() => {
+                // Optional: Reload the page after navigation if needed
+               window.location.reload();
+              });
+            },
+            error: (error) => {
+              console.error('Logout error:', error);
+            }
+          });
+        }
+        return throwError(()=>error);
+      })
+    ); // Pass the cloned request with headers to the next handler
   }
 }
