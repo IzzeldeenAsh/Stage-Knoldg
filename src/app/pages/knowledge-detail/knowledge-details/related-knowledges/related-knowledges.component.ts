7ef8a5704b29c5c51f7d91f9b77085eeeb028c1f
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-01-25 18:24:42 +0300

    uods

--- src/app/pages/knowledge-detail/knowledge-details/related-knowledges/related-knowledges.component.ts
+++ src/app/pages/knowledge-detail/knowledge-details/related-knowledges/related-knowledges.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
 @Component({
   selector: 'app-related-knowledges',
   templateUrl: './related-knowledges.component.html',
-  styleUrl: './related-knowledges.component.scss'
+  styleUrls: ['./related-knowledges.component.scss']
 })
-export class RelatedKnowledgesComponent {
+export class RelatedKnowledgesComponent implements OnInit, AfterViewInit {
+  private swiper: Swiper;
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
+
+  private initSwiper(): void {
+    this.swiper = new Swiper('.swiper-container', {
+      modules: [Navigation, Pagination],
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      }
+    });
+  }
 }
