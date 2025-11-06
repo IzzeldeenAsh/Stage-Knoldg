9e3880acf18d8366fbd22588930b0b6731a865b0
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-11-06 15:47:54 +0300

    fsdfsd

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -142,7 +142,7 @@ export class LoginComponent extends BaseComponent implements OnInit, OnDestroy {
     let returnUrl = this.returnUrl !== "/" ? this.returnUrl : document.referrer;
     
     // Check if returnUrl is a full URL from allowed domains
-    if (returnUrl && (returnUrl.includes('insightabusiness.com') || returnUrl.includes('localhost'))) {
+    if (returnUrl && (returnUrl.includes('foresighta.co') || returnUrl.includes('localhost'))) {
       return returnUrl;
     }
     
