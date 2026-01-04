31c6f67e9da0c3827c8592f2fe8279da63a0fc80
Izzeldeen Ashour <ezraashour00@gmail.com>
2026-01-04 18:18:23 +0300

    up

--- src/app/pipes/notifications-icons/notificaitons-icons.pipe.ts
+++ src/app/pipes/notifications-icons/notificaitons-icons.pipe.ts
@@ -8,38 +8,33 @@ export class NotificationsIconsPipe implements PipeTransform {
   transform(type: string): string {
     switch (type) {
       case 'knowledge': // used as sub_type for order notifications
-        return 'duotune/notificaitons/knowledge.svg';
+        return 'duotune/finance/fin010.svg';
       case 'deactivate_company':
       case 'deactivate_company_with_delete':
       case 'deactivate_insighter':
       case 'deactivate_insighter_with_delete':
-        return 'duotune/notificaitons/deactivate.svg';
+        return 'duotune/general/gen047.svg';
       case 'activate_company':
       case 'activate_insighter':
-        return 'duotune/notificaitons/activate.svg';
-      case 'declined':
-      case 'knowledge_declined':
-          return 'duotune/notificaitons/decline.svg';
+        return 'duotune/arrows/arr086.svg';
       case 'accept_knowledge':
-        return 'duotune/notificaitons/accept_knlg.svg';
+        return 'duotune/files/fil025.svg';
       case 'client_meeting_insighter_approved':
-        return 'duotune/notificaitons/accept_mtg.svg';
       case 'client_meeting_new':
-        return 'duotune/notificaitons/mtg.svg';
+        return 'duotune/general/gen014.svg';
       case 'insighter_meeting_client_new':
-         return 'duotune/notificaitons/mtg.svg'
       case 'insighter_meeting_approved':
-        return 'duotune/notificaitons/accept_mtg.svg'
+        return 'duotune/general/gen014.svg'
       case 'client_meeting_insighter_postponed':
-        return 'duotune/notificaitons/postp_mtg.svg';
+        return 'duotune/general/gen014.svg';
       case 'client_meeting_reschedule':
-        return 'duotune/notificaitons/mtg.svg';
+        return 'duotune/general/gen014.svg';
       case 'insighter_meeting_client_reschedule':
-        return 'duotune/notificaitons/mtg.svg';
+        return 'duotune/general/gen014.svg';
       case 'insighter_meeting_reminder':
-        return 'duotune/notificaitons/mtg.svg';
+        return 'duotune/general/gen014.svg';
       case 'client_meeting_reminder':
-        return 'duotune/notificaitons/mtg.svg';
+        return 'duotune/general/gen014.svg';
       default:
         return 'duotune/general/gen007.svg';
     }
